2bed7c6ef6f9cae98dbb0dc34b4b632ddd4e13e1
Корховой Вадим <veider@inbox.ru>
2021-12-08 21:21:35 +0300

    Домашнее задание за 06.12.2021

--- app/src/main/java/ru/gb/android_acquaintance/MainActivity.java
+++ app/src/main/java/ru/gb/android_acquaintance/MainActivity.java
@@ -3,12 +3,15 @@ package ru.gb.android_acquaintance;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(v -> setContentView(R.layout.activity_second));
     }
 }
